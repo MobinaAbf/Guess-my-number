930b6fad6c1985cac1d850f6b364648fa4df590e
Mobina Abforoosh <mobinaabf@gmail.com>
2023-09-13 12:16:28 +0330

    Add a condition to enter the number

--- script.js
+++ script.js
@@ -5,19 +5,23 @@ let score = 20;
 
 document.querySelector(".check").addEventListener("click", function () {
   const guess = Number(document.querySelector(".guess-num").value);
-  if (num === guess) {
-    document.querySelector(".message").textContent = " Correct Number :)";
-    document.body.style.background = "#4caf50";
-    document.querySelector(".number").textContent = num;
+  if (!guess) {
+    document.querySelector(".message").textContent = "No number !";
   } else {
-    if (num > guess) {
-      document.querySelector(".message").textContent = " Too low!";
-      score--;
-      document.querySelector(".score").textContent= score;
+    if (num === guess) {
+      document.querySelector(".message").textContent = " Correct Number :)";
+      document.body.style.background = "#4caf50";
+      document.querySelector(".number").textContent = num;
     } else {
-      document.querySelector(".message").textContent = " Too high!";
-      score--;
-      document.querySelector(".score").textContent = score;
+      if (num > guess) {
+        document.querySelector(".message").textContent = " Too low!";
+        score--;
+        document.querySelector(".score").textContent = score;
+      } else {
+        document.querySelector(".message").textContent = " Too high!";
+        score--;
+        document.querySelector(".score").textContent = score;
+      }
     }
   }
 });
