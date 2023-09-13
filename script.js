360e4ea58d642e47d3b57abc6ba26ad9c4ded776
Mobina Abforoosh <mobinaabf@gmail.com>
2023-09-13 12:30:42 +0330

    Losing in 0 score

--- script.js
+++ script.js
@@ -13,14 +13,19 @@ document.querySelector(".check").addEventListener("click", function () {
       document.body.style.background = "#4caf50";
       document.querySelector(".number").textContent = num;
     } else {
-      if (num > guess) {
-        document.querySelector(".message").textContent = " Too low!";
-        score--;
-        document.querySelector(".score").textContent = score;
+      if (score > 0) {
+        if (num > guess) {
+          document.querySelector(".message").textContent = " Too low!";
+          score--;
+          document.querySelector(".score").textContent = score;
+        } else {
+          document.querySelector(".message").textContent = " Too high!";
+          score--;
+          document.querySelector(".score").textContent = score;
+        }
       } else {
-        document.querySelector(".message").textContent = " Too high!";
-        score--;
-        document.querySelector(".score").textContent = score;
+        document.querySelector(".message").textContent = " You lost :(";
+        document.body.style.background = "#d92525";
       }
     }
   }
