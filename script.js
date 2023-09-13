9e7608db81e869de92e73433cca0fe6aaed99895
Mobina Abforoosh <mobinaabf@gmail.com>
2023-09-13 10:17:59 +0330

    If the correct number is entered in the div tag, the number will be displayed

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 "use strict";
 let num = Math.trunc(Math.random() * 20 + 1);
+console.log(num);
 
 document.querySelector(".check").addEventListener("click", function () {
   const guess = Number(document.querySelector(".guess-num").value);
   if (num === guess) {
     document.querySelector(".message").textContent = " Correct Number :)";
+    document.body.style.background = "#4caf50";
+    document.querySelector(".number").textContent = num;
   } else {
     if (num > guess) {
       document.querySelector(".message").textContent = " Too low!";
