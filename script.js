7952bcaa63c83bdd0ce84028b7e2ecf7caea7953
Mobina Abforoosh <mobinaabf@gmail.com>
2023-09-13 10:27:30 +0330

    Decreasing the score if the wrong number is entered

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 "use strict";
 let num = Math.trunc(Math.random() * 20 + 1);
 console.log(num);
+let score = 20;
 
 document.querySelector(".check").addEventListener("click", function () {
   const guess = Number(document.querySelector(".guess-num").value);
@@ -11,8 +12,12 @@ document.querySelector(".check").addEventListener("click", function () {
   } else {
     if (num > guess) {
       document.querySelector(".message").textContent = " Too low!";
+      score--;
+      document.querySelector(".score").textContent= score;
     } else {
       document.querySelector(".message").textContent = " Too high!";
+      score--;
+      document.querySelector(".score").textContent = score;
     }
   }
 });
