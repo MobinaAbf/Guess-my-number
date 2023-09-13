18796bb581214b791df186c817e2e4a8ad8ec695
Mobina Abforoosh <mobinaabf@gmail.com>
2023-09-13 10:12:37 +0330

    First, create a random number using the math method, then use the querySelector method to select the click button and use the addEventListener method and create a function to compare the entered number and the random number and change the background if the correct number is entered.

--- script.js
+++ script.js
@@ -1,2 +1,15 @@
 "use strict";
+let num = Math.trunc(Math.random() * 20 + 1);
 
+document.querySelector(".check").addEventListener("click", function () {
+  const guess = Number(document.querySelector(".guess-num").value);
+  if (num === guess) {
+    document.querySelector(".message").textContent = " Correct Number :)";
+  } else {
+    if (num > guess) {
+      document.querySelector(".message").textContent = " Too low!";
+    } else {
+      document.querySelector(".message").textContent = " Too high!";
+    }
+  }
+});
